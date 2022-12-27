b56fd97860dc8cddbc36d7ac35e296a2269089c0
jokessenich <johnny.kessenich@gmail.com>
2022-12-26 22:02:22 -0500

    updates to home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -14,16 +14,18 @@ export default class Home extends React.Component {
                 <h3 className="breathe">breathe</h3>
                 <NavBar></NavBar>
                 <Link to='/' className="entryheader">Imagery for ED</Link>
+                <a className="scheduleLinkHome" href="https://calendly.com/innermythology/intro-session">Book a Free Session!</a>
+
                 {//<p className='p-subheader'> Relax | Explore | Thank</p>
     }
                 </div>
 
                 <div className="bottom-half">
-                    <p className="p-introduction">
+                    <p className="p-introduction">ED is a condition that is often due to an issue in the relationship between the conscious and subconscious minds.
                     Everyone has a subconscious and a conscious mind. The subconscious mind is what keeps us alive, beats our heart, breaths air without us knowing, digests our food, and arouses us sexually. The conscious mind is the internal dialogue that we are aware of inside of our minds. 
-                    {<br />}{<br />}ED is a condition that is sometimes caused by an incongruence in the subconscious and conscious minds. Other times, ED can be caused by a negative reinforcement cycle (which feels like a negative spiral) between the conscious mind and subconscious mind.
+                    {<br />}{<br />}A simplistic example of how this works with ED is that the conscious mind wants to have a sexual arousal, but the subconscious mind refuses. This refusal could be due to fear, anger, feelings of neglect, among many other things. These feelings grow because of the incongruence and frustration in the subconscious and conscious minds. We have now entered a negative reinforcement cycle (which feels like a negative spiral) between the conscious mind and subconscious mind.
                     {<br />}{<br />}
-Interactive Guided Imagery is a technique that allows participants to dialogue with their subconscious mind and learn to work together with their subconscious. This technique is non-invasive and virtually no-risk. The introductory session is free. I welcome you to give it a try.
+Interactive Guided Imagery is a technique that allows participants to dialogue with their subconscious mind and learn to work together with their subconscious. We use a technique called Inner Exploration and see what the subconscious wants and needs and how to heal. We bring it gifts and gratitude and start restoring the love in the relationship of subconscious and conscious. This technique is non-invasive and virtually no-risk. The introductory session is free. I welcome you to give it a try.
 
                     {<br />}{<br />}
 
